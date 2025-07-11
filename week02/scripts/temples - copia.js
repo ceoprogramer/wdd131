6a94466e159fa360e515cd7b1be3c6ab75a85adb
ceoprogramer <esanchezsolano@byupathway.edu>
2025-07-11 16:17:46 -0600

    update menu

---
+++ week02/scripts/temples - copia.js
@@ -0,0 +1,7 @@
+const temButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("_______", () => {
+	navigation._______.toggle("_______");
+	temButton.classList.toggle("open");
+});
