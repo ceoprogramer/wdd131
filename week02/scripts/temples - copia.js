7324b5486f323a0b1c485ab4c2439b296c7848be
ceoprogramer <esanchezsolano@byupathway.edu>
2025-07-19 14:39:43 -0600

    create responsive place page

--- week02/scripts/temples - copia.js
+++
@@ -1,7 +0,0 @@
-const temButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
-
-hamButton.addEventListener("_______", () => {
-	navigation._______.toggle("_______");
-	temButton.classList.toggle("open");
-});
