37a13a7f557e2a79414f074e8478d8afa4f7b49a
ceoprogramer <esanchezsolano@byupathway.edu>
2025-08-12 14:45:34 -0600

    update paths

--- project/yucatan-magico/scripts/filtered-places.js
+++ project/yucatan-magico/scripts/filtered-places.js
@@ -2,7 +2,6 @@ const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 const resGrid = document.querySelector('.res-grid'); // Select div to display temple cards
 
-
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show'); // class 'show' in UL nav
     hambutton.classList.toggle('show'); // class 'show' in hamburger button
